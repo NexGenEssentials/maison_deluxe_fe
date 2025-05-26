2166bf8b6246997d237e1ede87472f03cf131e6c
Wilbrord <bwilbrord@gmail.com>
2025-05-26 17:26:02 +0200

    testimonial responsiveness

--- app/components/common/testimonia.tsx
+++ app/components/common/testimonia.tsx
@@ -28,7 +28,7 @@ const Testimonials = () => {
 
   return (
     <section className="relative py-20 bg-primaryBlue text-white text-center">
-      <div className="bg-gradient-to-r from-transparent via-[#100699]/40 via-50%  py-12  to-transparent px-4">
+      <div className="bg-gradient-to-r from-transparent via-[#100699]/10 via-50%  py-12  to-transparent px-4">
         <Title title="What Our Customers say" />
         <p className="text-sm md:text-base text-gray-300 mb-12">
           Praesent volutpat augue sapien sapien. A sit neque viverra eleifend
@@ -41,29 +41,44 @@ const Testimonials = () => {
         <Swiper
           modules={[Autoplay, Keyboard]}
           spaceBetween={50}
-          slidesPerView={3}
+          slidesPerView={2.5}
           centeredSlides={true}
           loop={true}
           grabCursor={true}
           autoplay={{
-            delay: 400000,
+            delay: 4000,
             disableOnInteraction: false,
           }}
           keyboard={{
             enabled: true,
           }}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            640: {
+              slidesPerView: 1.2,
+              spaceBetween: 30,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+          }}
           className="w-full py-10  "
           onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
         >
           {testimonials.map((t, i) => (
-            <SwiperSlide
-              key={i}
-              className=" py-20 relative flex justify-center"
-            >
+            <SwiperSlide key={i} className="py-40 relative flex justify-center">
               <div
-                className={`transition-all overflow-hidden  duration-300 flex items-center gap-6 rounded-xl  text-left ${
+                className={`max-lg:hidden transition-all overflow-hidden  duration-300 flex items-center gap-6 rounded-xl  text-left ${
                   activeIndex === i
-                    ? " rounded-bl-[80px] min-w-xl scale-[1.4] z-10 bg-secondaryBlue shadow-[#100699]/40 backdrop-blur-2xl border border-[#7576C3] shadow-2xl"
+                    ? " rounded-bl-[80px]  scale-[1.4] z-10 bg-secondaryBlue shadow-[#100699]/80 backdrop-blur-2xl border border-[#7576C3] shadow-2xl"
                     : "opacity-70 bg:white/10 blur-[2px] "
                 }`}
               >
@@ -83,10 +98,12 @@ const Testimonials = () => {
                   <h3 className="text-xl font-bold text-lime-300 mb-2">
                     {t.title}
                   </h3>
-                  <p className="text-base text-white mb-4">{t.description}</p>
+                  <p className="text-base text-[#C7C7FF] mb-4">
+                    {t.description}
+                  </p>
                   <div className="flex justify-between flex-wrap gap-4 items-center">
                     <StarRating rating={t.rating} />
-                    <div className="text-sm text-gray-300">
+                    <div className="text-sm text-[#C7C7FF]">
                       {t.name} – {t.country}
                       <Image
                         src={t.flag}
@@ -99,6 +116,50 @@ const Testimonials = () => {
                   </div>
                 </div>
               </div>
+
+              <div
+                className={` lg:hidden transition-all rounded-2xl p-8 overflow-hidden duration-300 flex flex-col gap-12  text-left ${
+                  activeIndex === i
+                    ? "z-10 bg-secondaryBlue shadow-[#100699]/80 backdrop-blur-2xl border border-[#7576C3] shadow-2xl"
+                    : "opacity-70 bg:white/10 blur-[2px] "
+                }`}
+              >
+                <div className=" flex items-center gap-4">
+                  <div className="relative w-24 h-24 overflow-hidden">
+                    <Image
+                      src={t.image}
+                      alt={t.name}
+                      fill
+                      className={`object-cover h-full rounded-full`}
+                    />
+                  </div>
+                  <div className="flex-1 ">
+                    <h3 className="text-xl font-bold text-lime-300 mb-2">
+                      {t.title}
+                    </h3>
+                    <div className="text-sm text-[#C7C7FF] ">
+                      {t.name} – {t.country}
+                      <Image
+                        src={t.flag}
+                        alt={t.country}
+                        width={18}
+                        height={12}
+                        className="inline ml-1"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex-1">
+                  <p className="text-base text-[#C7C7FF] mb-4">
+                    {t.description}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="text-xs text-[#C7C7FF]"> Ratings</span>
+                  <StarRating rating={t.rating} />
+                </div>
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -108,7 +169,7 @@ const Testimonials = () => {
           {testimonials.map((t, i) => (
             <div
               key={i}
-              className="relative  overflow-hidden rounded-full border-4 border-[#DFDDFC4D] -ml-6 first:ml-0"
+              className="relative  overflow-hidden rounded-full border-4 border-[#DFDDFC4D] -ml-10 first:ml-0"
             >
               <div
                 key={i}
