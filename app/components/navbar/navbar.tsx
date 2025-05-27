286e63ab4feedfb49c1a707d9840597cd6a1c7a9
Wilbrord <bwilbrord@gmail.com>
2025-05-27 03:21:33 +0200

    adding mobile navbar"

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import whiteLogo from "@/public/images/white_logo.png";
+import redLogo from "@/public/images/red_logo.png";
 import { CiMenuFries } from "react-icons/ci";
 import Link from "next/link";
+import { FaAngleRight } from "react-icons/fa";
 
 const navbarLinks = [
-  { name: "Rooms", href: "#rooms" },
+  { name: "Rooms", href: "/rooms" },
   { name: "About", href: "#about" },
   { name: "Blog", href: "#blog" },
   { name: "Contact", href: "#contact" },
 ];
 
 const Navbar = () => {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    console.log("Navbar toggled");
+    setOpenNavbar(!openNavbar);
+  };
   return (
     <>
       {/* large screen navbar */}
@@ -39,15 +48,49 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="max-lg:flex hidden w-full px-6 py-4  items-center justify-between text-white font-medium ">
+      {/* Mobile screen navbar */}
+      <div className="relative max-lg:flex hidden w-full px-6 py-4 items-center justify-between text-white font-medium">
         <Image src={whiteLogo} width={52} height={56} alt="White Logo" />
+
         <div className="hover:bg-primaryGreen/15 duration-200 transition font-bold text-white p-4 rounded cursor-pointer">
-          <CiMenuFries size={24} />
+          <CiMenuFries onClick={toggleNavbar} size={24} />
         </div>
-        {/* <li>Rooms</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li> */}
+
+        <div
+          className={`absolute top-0 right-0 left-0 w-full z-50 bg-[#000000cc] backdrop-blur-[5px] h-[80vh] transition-all duration-500 ease-in-out ${
+            openNavbar
+              ? "translate-y-0 opacity-100"
+              : "-translate-y-full opacity-0 pointer-events-none"
+          }`}
+        >
+          <div className="flex justify-between items-center p-6 bg-white">
+            <Image src={redLogo} width={52} height={56} alt="White Logo" />
+            <button
+              onClick={toggleNavbar}
+              className="text-black text-2xl font-bold"
+            >
+              &#x2715;
+            </button>
+          </div>
+          <ul className="flex flex-col gap-8 text-white font-[Josefin_Slab] text-xl p-10">
+            {navbarLinks.map((link) => (
+              <Link key={link.name} href={link.href}>
+                <li
+                  onClick={() => setOpenNavbar(false)}
+                  className="hover:text-primaryGreen hover:bg-gray-100/10 flex items-center justify-between p-4 rounded-md cursor-pointer transition-all duration-300"
+                >
+                  {link.name}
+                  <FaAngleRight />
+                </li>
+              </Link>
+            ))}
+          </ul>
+          {/* <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+            <button className="px-8 py-3 bg-white rounded-lg text-primaryGreen font-bold tracking-wide">
+              ENG
+            </button>
+          </div> */}
+        </div>
       </div>
     </>
   );
