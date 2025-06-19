e3ad16e22d54033ae5ccc3c16d45cefdcbe9e1ba
Wilbrord <bwilbrord@gmail.com>
2025-06-19 15:07:25 +0200

    admin part

--- app/dashboard/components/sideBarNav.tsx
+++ app/dashboard/components/sideBarNav.tsx
@@ -1,13 +1,10 @@
 "use client";
 import React, { useState } from "react";
-import {
-  FiHome,
-  FiCalendar,
-  FiBriefcase,
-  FiMessageSquare,
-} from "react-icons/fi";
+import { FiHome, FiBriefcase, FiMessageSquare } from "react-icons/fi";
 import clsx from "clsx";
 import { useAppContext } from "@/app/context";
+import { RiLogoutCircleLine } from "react-icons/ri";
+import { useRouter } from "next/navigation";
 
 const menuItems = [
   {
@@ -30,6 +27,7 @@ const menuItems = [
 const SideBarNav = () => {
   const [active, setActive] = useState("Dashboard");
   const { setActiveTab } = useAppContext();
+  const router = useRouter();
 
   return (
     <aside className="bg-[#F4F7FF] h-screen fixed top-25 left-0 min-w-[240px] pt-10 shadow-md">
@@ -52,6 +50,16 @@ const SideBarNav = () => {
             <span className="">{item.label}</span>
           </button>
         ))}
+        <div
+          onClick={() => router.push("/")}
+          className="hover:bg-white hover:text-primaryBlue hover:font-semibold hover:shadow cursor-pointer flex items-center gap-3 px-4 py-2 rounded-lg text-sm font-medium text-gray-700 transition-all duration-200"
+        >
+          {" "}
+          <span className="text-xl">
+            <RiLogoutCircleLine />
+          </span>
+          <span className="">Logout</span>
+        </div>
       </nav>
     </aside>
   );
--- app/dashboard/components/topbar.tsx
+++ app/dashboard/components/topbar.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Image from "next/image";
 import redlogo from "@/public/images/red_logo.png";
 import { FiSearch, FiBell, FiInfo, FiGlobe } from "react-icons/fi";
+import Link from "next/link";
 
 const Topbar = () => {
   return (
     <div className="sticky top-0 left-0 z-50 bg-white flex justify-between items-center w-full p-6 shadow-sm">
-      <div className="w-16 h-16 relative">
+      <Link href={"/"} className="w-16 h-16 relative">
         <Image src={redlogo} alt="Description of image" fill />
-      </div>
+      </Link>
       <div className="flex items-center bg-white rounded-full  px-4 py-2 space-x-4 ">
         {/* Search input */}
         <div className="flex items-center bg-[#F6F8FF] rounded-full px-4 py-2 w-full min-w-[300px]">
